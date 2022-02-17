640163c8dada07555f7f2caf70e73e1a28567492
AzmiHabib <azmihabib0323@gmail.com>
2022-02-17 14:27:07 +0600

    js error handle

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-function callWithId(idname){
+function callWithId(idname) {
     return document.getElementById(idname)
 }
-function inputValue(inputId){
-    const incomeInput = callWithId(inputId).value;
+
+function errorText(text){
+    callWithId('modal-text').innerText = text
+        callWithId('first-modal').style.display = 'block';
 }
-callWithId('calculate-btn').addEventListener('click',function(){
-// get calculate button 
-    const incomeInput = callWithId('income-input').value;
-    const foodInput = callWithId('food-input').value;
-    const rentInput = callWithId('rent-input').value;
-    const clothesInput =callWithId('clothes-input').value;
+
+let balances = 0;
+let incomeInput = 0;
+callWithId('calculate-btn').addEventListener('click', function () {
+    // get calculate button 
+     incomeInput = parseInt(callWithId('income-input').value);
+    const foodInput = parseInt(callWithId('food-input').value);
+    const rentInput = parseInt(callWithId('rent-input').value);
+    const clothesInput = parseInt(callWithId('clothes-input').value);
     const totalExpences = callWithId('total-expences');
     const balanceInfo = callWithId('balance-info');
 
-    if(incomeInput<0 || foodInput<0 || rentInput<0 || clothesInput<0){
-        callWithId('modal-text').innerText = "Please Enter Positive Numbers"
-        callWithId('first-modal').style.display = 'block';
+    if (incomeInput < 0 || foodInput < 0 || rentInput < 0 || clothesInput < 0) {
+        errorText("Please Enter Positive Numbers")
     }
-    else if(isNaN(incomeInput) == true|| isNaN(foodInput) == true || isNaN(rentInput) == true || isNaN(clothesInput) ==true){
-        callWithId('modal-text').innerText = "Plese Fillup all with number"
-        callWithId('first-modal').style.display = 'block';
+    else if (isNaN(incomeInput) == true || isNaN(foodInput) == true || isNaN(rentInput) == true || isNaN(clothesInput) == true) {
+        errorText("Plese Fillup with all valid number")
     }
-    // else if(incomeInput ||foodInput||rentInput||clothesInput){
-    //     callWithId('modal-text').innerText = "Plese Fillup all inputs"
-    //     callWithId('first-modal').style.display = 'block';
-    // }
-    const allExpence = (parseInt(foodInput)+parseInt(rentInput)+parseInt(clothesInput));
+
+    const allExpence = foodInput + rentInput + clothesInput;
     totalExpences.innerText = allExpence;
-    balanceInfo.innerText = parseInt(incomeInput) - allExpence
-    
+    balances = incomeInput - allExpence
+    balanceInfo.innerText = balances;
+
 })
 
 // savings calculation 
-callWithId('save-btn').addEventListener('click',function(){
-    const incomeInput = document.getElementById('income-input').value;
-    const saveInfo =callWithId('save-info').value;
+callWithId('save-btn').addEventListener('click', function () {
+    const saveInfo = parseInt(callWithId('save-info').value);
     const savingAmmount = callWithId('saving-ammount');
     const remainBalance = callWithId('remain-balance');
-    const balanceInfo = callWithId('balance-info');
-    const finalSavingAmmount = (parseInt(incomeInput) * parseInt(saveInfo))/100
-    savingAmmount.innerText = finalSavingAmmount;
-    remainBalance.innerText = parseFloat(balanceInfo.innerText)-parseInt(finalSavingAmmount);
-
+    let totalSavings = (incomeInput * saveInfo) / 100;
+    
+    if(saveInfo<0 || isNaN(saveInfo)){
+        errorText( "Please Enter Positive Savings or valid number")
+    }else if(balances<totalSavings){
+        errorText( "you have not enough money to save")
+    }else{
+        savingAmmount.innerText = totalSavings
+        remainBalance.innerText = balances - totalSavings
+    }
 })
+//close first modal
 callWithId('first-modal').addEventListener('click', function () {
     callWithId('first-modal').style.display = 'none'
 })
